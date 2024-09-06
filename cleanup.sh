601fbd55086aaf6eff3c1dd001c42775fd61595e
shyu61 <ohs6261@gmail.com>
2024-09-06 19:13:52 +0900

    Update

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-n=$(find ./code/problems -type f -name 'main*.cpp' | sed -E 's/.*main([0-9]+)\.cpp/\1/' | sort -n | tail -1)
+target=${1:-problems}
+
+n=$(find ./code/${target} -type f -name 'main*.cpp' | sed -E 's/.*main([0-9]+)\.cpp/\1/' | sort -n | tail -1)
 filename=$((n+1))
 
-mv main.cpp ./code/problems/main${filename}.cpp
+mv main.cpp ./code/${target}/main${filename}.cpp
 cp base.cpp main.cpp
 rm -f main
