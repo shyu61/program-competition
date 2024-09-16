2ee157588e0d1c14340687896cad042157d6042a
shyu61 <ohs6261@gmail.com>
2024-09-17 00:28:10 +0900

    Update

--- cleanup.sh
+++ cleanup.sh
@@ -2,8 +2,9 @@
 
 target=${1:-problems}
 
-n=$(find ./code/${target} -type f -name 'main*.cpp' | sed -E 's/.*main([0-9]+)\.cpp/\1/' | sort -n | tail -1)
-filename=$((n+1))
+last_num=$(find ./code/${target} -type f -name 'main*.cpp' | sed -E 's/.*main([0-9]*)\.cpp/\1/' | sort -n | tail -1)
+new_number=$((10#$last_num+1))
+filename=$(printf "%03d" ${new_number})
 
 mv main.cpp ./code/${target}/main${filename}.cpp
 cp base.cpp main.cpp
---
+++ code/problems/main023.cpp
@@ -0,0 +1,55 @@
+// E - Minimize Sum of Distances
+// https://atcoder.jp/contests/abc348/tasks/abc348_e
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int n;
+ll total = 0;
+vector<vector<int>> G;
+vector<int> c, centroids;
+vector<ll> dp;
+
+void getCentroids(int v, int p) {
+    dp[v] = c[v];
+    bool is_centroid = true;
+    for (auto ch : G[v]) {
+        if (ch == p) continue;
+        getCentroids(ch, v);
+        if (dp[ch] > total / 2) is_centroid = false;
+        dp[v] += dp[ch];
+    }
+    if (total - dp[v] > total / 2) is_centroid = false;
+    if (is_centroid) centroids.push_back(v);
+}
+
+void dfs(ll& ans, int v, int p, int d) {
+    ans += ll(d) * c[v];
+    for (auto ch : G[v]) {
+        if (ch == p) continue;
+        dfs(ans, ch, v, d + 1);
+    }
+}
+
+int main() {
+    cin >> n;
+    G = vector<vector<int>>(n, vector<int>());
+    c = vector<int>(n);
+    dp = vector<ll>(n);
+    for (int i = 0; i < n - 1; i++) {
+        int a, b; cin >> a >> b;
+        a--; b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+        total += c[i];
+    }
+
+    getCentroids(0, -1);
+
+    ll ans = 0;
+    dfs(ans, centroids[0], -1, 0);
+    cout << ans << endl;
+}
