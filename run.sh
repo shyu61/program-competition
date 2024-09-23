81b49a5a025c8497e43a6bf83f8b4c966bf929ab
shyu61 <ohs6261@gmail.com>
2024-09-23 23:20:28 +0900

    Update

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+g++-14 -std=c++17 -I . -g -o main main.cpp
+
+awk 'BEGIN {RS="\n\n"} {f="chunk" NR ".txt"; print > f}' in.txt
+
+for chunk in chunk*.txt; do
+    cat "$chunk" | ./main
+done
+
+rm -r chunk*.txt
