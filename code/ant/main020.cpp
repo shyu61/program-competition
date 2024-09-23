03bbdafe3a7d4eff7ddc408d83a969305307ccab
shyu61 <ohs6261@gmail.com>
2024-09-24 02:28:20 +0900

    Update

--- code/ant/main020.cpp
+++ code/ant/main020.cpp
@@ -7,18 +7,19 @@ const int INF = 1e9 + 1;
 int n, k;
 vector<int> a;
 
-deque<int> deq;
+deque<pair<int, int>> deq;
 
-void push(int v) {
+void push(int v, int i) {
     // 常に先頭に最小値が来るように処理する
     // 末尾からpopしながら進むことで、範囲外要素は必ずpushがgetのどちらかで除去される
-    while (!deq.empty() && deq.back() >= v) deq.pop_back();
-    deq.push_back(v);
+    while (!deq.empty() && deq.back().first >= v) deq.pop_back();
+    deq.emplace_back(v, i);
 }
 
-int get() {
-    int v = deq.front();
-    deq.pop_front();
+int get(int left) {
+    auto [v, i] = deq.front();
+    // 使った先頭がキャンセル対象の場合はpopする
+    if (i == left) deq.pop_front();
     return v;
 }
 
@@ -29,8 +30,8 @@ int main() {
 
     vector<int> b;
     for (int i = 0; i < n; i++) {
-        push(a[i]);
-        if (i >= k - 1) b.push_back(get());
+        push(a[i], i);
+        if (i >= k - 1) b.push_back(get(i - k + 1));
     }
 
     for (auto x : b) cout << x << " ";
