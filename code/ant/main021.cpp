1d64ec9e10bf014b6d4b345ac050aa60b7317d3e
shyu61 <ohs6261@gmail.com>
2024-09-24 17:58:31 +0900

    Update

--- code/ant/main021.cpp
+++ code/ant/main021.cpp
@@ -1,27 +1,28 @@
-// 尺取り法: p135
+// デックの利用: スライド最小値: p300
+// dequeを使わないプレーンな尺取り法での実装
 #include <bits/stdc++.h>
 using namespace std;
 
-// 尺取り法の本質は、重複部分の結果は使い回し、範囲外になった部分はキャンセル、新規に範囲になる部分は追加計算する。
-// 連続区間に対するもので、全体で単調性が成立する時のみ使える
-// 最小/最大個数, 合計値, 最大/最小値(最大/最小になる値のindexが単調増加する)
-
 int main() {
-    int n, S; cin >> n >> S;
+    int n, k;
+    cin >> n >> k;
     vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
 
-    int ans = n + 1;
-    int sum = 0, right = 0;
-    for (int left = 0; left < n; left++) {
-        while (sum < S && right < n) sum += a[right++];
+    set<pair<int, int>> s;
+    for (int i = 0; i < k - 1; i++) s.emplace(a[i], i);
 
-        if (sum >= S) {
-            ans = min(ans, right - left);
-            sum -= a[left];
-        } else break;
+    vector<int> ans;
+    int right = k - 1;
+    for (int left = 0; left <= n - k; left++) {
+        s.emplace(a[right], right);
+        right++;
+
+        auto [v, i] = *s.begin();
+        ans.push_back(v);
+        if (i == left) s.erase(s.begin());
     }
 
-    if (ans > n) ans = -1; // 解なし
-    cout << ans << endl;
+    for (auto x : ans) cout << x << " ";
+    cout << endl;
 }
--- code/ant/main022.cpp
+++ code/ant/main022.cpp
@@ -1,37 +1,27 @@
-// 尺取り法: p137
+// 尺取り法: p135
 #include <bits/stdc++.h>
 using namespace std;
 
-// 区間における最小個数を求める問題
-// 個数は単調性が成立するので、尺取り法が使える
+// 尺取り法の本質は、重複部分の結果は使い回し、範囲外になった部分はキャンセル、新規に範囲になる部分は追加計算する。
+// 連続区間に対するもので、全体で単調性が成立する時のみ使える
+// 最小/最大個数, 合計値, 最大/最小値(最大/最小になる値のindexが単調増加する)
 
 int main() {
-    int n; cin >> n;
+    int n, S; cin >> n >> S;
     vector<int> a(n);
-    map<int, int> cnt;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        cnt[a[i]]++;
-    }
-
-    int m = cnt.size();
+    for (int i = 0; i < n; i++) cin >> a[i];
 
     int ans = n + 1;
-    map<int, int> cur;
-    int right = 0, nuniq = 0;
+    int sum = 0, right = 0;
     for (int left = 0; left < n; left++) {
-        while (nuniq < m && right < n) {
-            cur[a[right]]++;
-            if (cur[a[right]] == 1) nuniq++;
-            right++;
-        }
+        while (sum < S && right < n) sum += a[right++];
 
-        if (nuniq < m) break;
-        ans = min(ans, right - left);
-
-        cur[a[left]]--;
-        if (cur[a[left]] == 0) nuniq--;
+        if (sum >= S) {
+            ans = min(ans, right - left);
+            sum -= a[left];
+        } else break;
     }
 
+    if (ans > n) ans = -1; // 解なし
     cout << ans << endl;
 }
