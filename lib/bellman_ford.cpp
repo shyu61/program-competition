ec70772e882dba2ab2d1ecdeb749f1fbbdd95e97
shyu61 <ohs6261@gmail.com>
2024-11-03 16:30:00 +0900

    Update

--- lib/bellman_ford.cpp
+++ lib/bellman_ford.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ベルマンフォード法の一言まとめ:
+// ある頂点iへの最短路は、その隣接ノードjへの最短路d[j]+ejiによって作られ、
+// 1回の緩和操作で少なくとも1頂点の最短路は確定する性質を利用し、最大V-1回の緩和操作を行うことで単一始点最短路問題を解くというもの
+
 struct Edge {
     int to, cost;
     Edge(int to, int cost): to(to), cost(cost) {};
