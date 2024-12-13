16be7009bf69ceb4675d4143b0131e8ff7eaa1bd
shyu61 <ohs6261@gmail.com>
2024-12-14 01:09:51 +0900

    Update

---
+++ lib/string/manber_myers.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// suffix-arrayは辞書順で開始位置を値に持つ
+// ダブリングの一種だが、自身を再帰的に更新する典型的なダブリングとは異なり、saとrankで互いに更新し合うという構造を取っている
+// ソート済みのsaはほぼrankと同じ順序構造を持つが、イコールの場合を正確に表現できない。イコールかどうかは前後半に分割して比較する上で非常に重要なのでrankを別途計算する必要がある
+// ポイント
+// - 文字列のソートは前後半で分割して考えることができる
+
+int n, k;
+vector<int> rnk, tmp;
+
+// (rank[i], rank[i+k])と(rank[j], rank[j+k])を比較
+bool compare_sa(int i, int j) {
+    // イコール判定がこの分割比較の核なので、saだけでの再帰更新ができずrankが必要になる
+    if (rnk[i] != rnk[j]) return rnk[i] < rnk[j];
+    else {  // 前半k文字のrankが一致した場合は、後半k文字のrankをみる
+        int ri = i + k <= n ? rnk[i + k] : -1;
+        int rj = j + k <= n ? rnk[j + k] : -1;
+        return ri < rj;
+    }
+}
+
+// 文字列Sの接頭辞配列を構築
+void construct_sa(string S, vector<int>& sa) {
+    n = S.length();
+
+    // 最初は1文字、ランクは文字コードにすれば良い
+    for (int i = 0; i <= n; i++) {
+        sa[i] = i;
+        rnk[i] = i < n ? S[i] : -1;
+    }
+
+    // k文字についてソートされているところから、2k文字でソートする
+    for (k = 1; k <= n; k *= 2) {
+        // kでのrankを使って2kのsaを作る
+        sort(sa.begin(), sa.end(), compare_sa);
+
+        // 次のsortで使うランクの計算: 2kのsaを作るのには、kのrankが必要。つまりrankの方が更新が後になる
+        // 故に2kのrankを計算するのに2kでソート済みのsaを使うことができる
+        tmp[sa[0]] = 0;
+        for (int i = 1; i <= n; i++) {
+            // saは2kでソート済みなのでsa[i]がsa[i-1]よりもrankが小さくなることはない
+            // ただしソート順が後でもrankが等しくなることはあり得るので比較する
+            tmp[sa[i]] = tmp[sa[i - 1]] + (compare_sa(sa[i - 1], sa[i]) ? 1 : 0);
+        }
+        for (int i = 0; i <= n; i++) rnk[i] = tmp[i];
+    }
+}
