090f9aeed1b7526156d3fcdef3d6b53291f961fa
shyu61 <ohs6261@gmail.com>
2024-12-20 20:29:19 +0900

    Update

--- lib/mod.cpp
+++ lib/mod.cpp
@@ -6,44 +6,57 @@ int gcd(int a, int b) {
     return gcd(b, a % b);
 }
 
-int extgcd(int a, int b, int& x, int& y) {
-    if (b == 0) {
-        x = 1, y = 0;
-        return a;
+
+struct Mod {
+    int mod;
+    vector<int> fact;
+    bool is_fact_preped = false;
+
+    Mod(int mod): mod(mod) {}
+
+    int extgcd(int a, int b, int& x, int& y) {
+        if (b == 0) {
+            x = 1, y = 0;
+            return a;
+        }
+        int d = extgcd(b, a % b, y, x);
+        y -= (a / b) * x;
+        return d;
     }
-    int d = extgcd(a, b, y, x);
-    y -= (a / b) * x;
-    return d;
-}
 
-int mod_inverse(int a, int m) {
-    int x, y;
-    extgcd(a, m, x, y);
-    return ((x % m) + m) % m;
-}
+    // 逆元
+    int get_inv(int a, int m) {
+        int x, y;
+        extgcd(a, m, x, y);
+        return ((x % m) + m) % m;
+    }
 
-vector<int> fact;  // n<pまでのn! mod p のテーブル
-// n! = a*p^e
-int mod_fact(int n, int p, int& e) {
-    e = 0;
-    if (n == 0) return 1;
+    // 0<=n<p に対しての n! mod p テーブルの事前計算
+    void prep_fact() {
+        fact = vector<int>(mod);
+        fact[0] = 1;
+        for (int i = 1; i < mod; i++) fact[i] = fact[i - 1] * i % mod;
+        is_fact_preped = true;
+    }
 
-    // pの倍数部分を計算
-    int res = mod_fact(n / p, p, e);
-    e += n / p;
+    // n!
+    int get_fact(int n, int& e) {
+        if (!is_fact_preped) prep_fact();
+        e = 0;
+        if (n == 0) return 1;
+        int res = get_fact(n / mod, e);
+        e += n / mod;
 
-    // (p-1)!≡-1なので(p-1)!^(n/p)はn/pの偶奇だけで計算できる
-    // a mod pを返却
-    // a≡(-1)^(n/p) * (n mod p)! * (1*2*...*n/p) (mod p)
-    if (n / p % 2 != 0) return res * (p - fact[n % p]) % p;
-    return res * fact[n % p] % p;
-}
+        if (n / mod % 2 != 0) return res * (mod - fact[n % mod]) % mod;
+        return res * fact[n % mod] % mod;
+    }
 
-// nCk mod pを求める: O(log_p n)
-int mod_comb(int n, int k, int p) {
-    if (n < 0 || k < 0 || n < k) return 0;
-    int e1, e2, e3;
-    int a1 = mod_fact(n, p, e1), a2 = mod_fact(k, p, e2), a3 = mod_fact(n - k, p, e3);
-    if (e1 > e2 + e3) return 0;
-    return a1 * mod_inverse(a2 * a3 % p, p) % p;
-}
+    // nCk
+    int get_comb(int n, int k) {
+        if (n < 0 || k < 0 || n < k) return 0;
+        int e1, e2, e3;
+        int a1 = get_fact(n, e1), a2 = get_fact(k, e2), a3 = get_fact(n - k, e3);
+        if (e1 > e2 + e3) return 0;
+        return a1 * get_inv(a2 * a3 % mod, mod) % mod;
+    }
+};
