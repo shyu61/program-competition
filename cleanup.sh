2166baa5dbfbe102160514bba3a10d35f4bb69a0
shyu61 <ohs6261@gmail.com>
2024-12-22 19:14:36 +0900

    Update

--- cleanup.sh
+++ cleanup.sh
@@ -2,10 +2,10 @@
 
 target=${1:-problems}
 
-last_num=$(find ./code/${target} -type f -name 'main*.cpp' | sed -E 's/.*main([0-9]*)\.cpp/\1/' | sort -n | tail -1)
+last_num=$(find ./code/${target} -type f -name '*.cpp' | sed -E 's/.*([0-9]*)\.cpp/\1/' | sort -n | tail -1)
 new_number=$((10#$last_num+1))
 filename=$(printf "%03d" ${new_number})
 
-mv main.cpp ./code/${target}/main${filename}.cpp
+mv main.cpp ./code/${target}/${filename}.cpp
 cp base.cpp main.cpp
 rm -f main
