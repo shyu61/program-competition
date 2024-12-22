5c05127181bcf1574d8ac50da8298afa25347de5
shyu61 <ohs6261@gmail.com>
2024-12-22 19:24:04 +0900

    Update

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+set -eu
+
 target=${1:-problems}
+
+if [ $target = 'contest' ]; then
+    con=${2}
+    [ ! -d ./code/contest/${con} ] && mkdir ./code/contest/${con}
+    mv main.cpp ./code/contest/${con}/${3}.cpp
+    cp base.cpp main.cpp
+    rm -f main
+    exit 0
+fi
 
 last_num=$(find ./code/${target} -type f -name '*.cpp' | sed -E 's/.*([0-9]*)\.cpp/\1/' | sort -n | tail -1)
 new_number=$((10#$last_num+1))
