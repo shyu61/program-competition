2185a7ef5053e649bb4881c4f8c7e6954efe18d9
shyu61 <ohs6261@gmail.com>
2024-12-23 23:02:03 +0900

    Update

---
+++ code/contest/abc385/a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c; cin >> a >> b >> c;
+    if (a == b && b == c) cout << "Yes" << endl;
+    else if (a + b == c || b + c == a || c + a == b) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
