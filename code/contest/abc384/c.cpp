9e2123e3445257d7e5f3a7a853cedbfaef947c90
shyu61 <ohs6261@gmail.com>
2024-12-29 19:56:26 +0900

    Update

--- code/contest/abc384/c.cpp
+++ code/contest/abc384/c.cpp
@@ -2,33 +2,29 @@
 using namespace std;
 using P = pair<int, string>;
 
+// sortの順序を制御する方法
+// - カスタムコンパレータを使う
+// - 符号を反転させる
+
 const int N = 5;
 
 int main() {
-    vector<int> score(N);
-    for (int i = 0; i < N; i++) cin >> score[i];
-
-    string S = "ABCDE";
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) cin >> a[i];
 
     vector<P> res;
-    for (int i = 1; i <= 1 << N; i++) {
-        int sum = 0;
-        string s = "";
+    for (int i = 1; i < 1 << N; i++) {
+        int score = 0; string name;
 
         for (int j = 0; j < N; j++) {
             if (i >> j & 1) {
-                sum += score[j];
-                s += S[j];
+                score += a[j];
+                name += "ABCDE"[j];
             }
         }
-        res.emplace_back(sum, s);
+        res.emplace_back(-score, name);
     }
 
-    sort(res.begin(), res.end(), [](const P& a, const P& b) {
-        if (a.first != b.first) return a.first > b.first;
-        return a.second < b.second;
-    });
-    for (auto p : res) {
-        cout << p.second << endl;
-    }
+    sort(res.begin(), res.end());
+    for (auto [score, name] : res) cout << name << endl;
 }
--- code/contest/abc384/d.cpp
+++ code/contest/abc384/d.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 using ll = long long;
 
+// ポイント
+// - 周期性がある問題は、剰余を取ることで周期の範囲内に還元することができる
+// - 周期性の問題は、周期を跨ぐ場合を考慮するために2倍長を考える
+//   - 時刻などを扱う問題も同様
+// - 累積和は開区間: S[i+1]はa[i]までの累積和を表し, S[0]は常に0
+
+// 累積和と尺取り法
+// 累積和: 調べたい区間が明確な場合, 区間幅が明確な場合
+// 尺取り法: 条件を満たす区間を逐次的に探索したい場合
+// -> 本問題は区間和がSになる区間の探索なので、条件と見れば尺取り法だし、区間幅がSと見れば累積和でもいい
+
 int main() {
     ll N, S; cin >> N >> S;
     vector<ll> A(N);
@@ -28,6 +39,29 @@ int main() {
         if (sum > S) {
             sum -= A[left % N];
         } else break;
+    }
+    cout << "No" << endl;
+}
+
+// 別解
+int main() {
+    ll N, S; cin >> N >> S;
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    // 累積和
+    vector<ll> s(N * 2 + 1);
+    for (int i = 0; i < N * 2; i++) s[i + 1] = s[i] + A[i % N];
+
+    S %= s[N];
+
+    set<ll> st;
+    for (auto si : s) st.insert(si);
+    for (auto si : s) {
+        if (st.count(si - S)) {
+            cout << "Yes" << endl;
+            return 0;
+        }
     }
     cout << "No" << endl;
 }
