0855caa5e7b1dbbf70e1f8481966ce83027d8234
shyu61 <ohs6261@gmail.com>
2024-12-30 23:00:52 +0900

    Update

--- code/contest/abc384/e.cpp
+++ code/contest/abc384/e.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ull = unsigned long long;
 using ll = long long;
+using T = tuple<ll, int, int>;
+
+// 思考フロー
+// グリッド問題, 逐次問題。逐次問題の色が特に強い。全探索が間に合わないので、貪欲法かdpかグラフを検討する。
+// -> 逐次問題はまず遷移条件を考えるところから始める。選択と吸収からなり、選択は最小のマスを常に選べば良いことには気づけるはず。
+// -> 常に最小とくればpriority_queue
 
 // priority_queueについて
 // - デフォルトではmax-heapなので降順(優先度が"高い")
 // - sortなどもそうだが、比較は基本lessで行われるので、`<`をオーバーライドする
+
+// ポイント
+// - 整数と割り算の結果を比較したい場合は、切り上げると整数値のまま比較ができる
+//   - 分母を払うと桁が大きくなりすぎる場合に特に有効
+//   - ⌈b/a⌉ = (b+a-1)/a を使う
+// - priority_queueやsortなどは、うまくpairやtupleの順番や符号を調整してデフォルのコンパレータが使えるようにすると楽
 
 // priority_queueとsetの計算量
 // - priority_queue
@@ -15,30 +26,17 @@ using ll = long long;
 //   - insert,delete: O(logN)
 //   - init: O(Nlogn)
 
-struct Point {
-    int x, y;
-    ll s;
-    Point(int x, int y, ll s) : x(x), y(y), s(s) {};
-    bool operator<(const Point &other) const {
-        return s > other.s;
-    };
-};
-
-vector<int> dx = {1,0,-1,0}, dy = {0,1,0,-1};
+array<int, 4> dx = {1,0,-1,0}, dy = {0,1,0,-1};
 int H, W, X, P, Q;
 vector<vector<ll>> S;
 
-void adj(int x, int y, priority_queue<Point>& pq) {
+void push(int x, int y, priority_queue<T, vector<T>, greater<T>>& pq) {
     for (int r = 0; r < 4; r++) {
         int nx = x + dx[r], ny = y + dy[r];
         if (nx >= 0 && nx < H && ny >= 0 && ny < W && S[nx][ny] != -1) {
-            pq.emplace(nx, ny, S[nx][ny]);
+            pq.emplace(S[nx][ny], nx, ny);
         }
     }
-}
-
-bool over18(ll a, ll b) {
-    return (to_string(a).size() - 1) + (to_string(b).size() - 1) >= 18;
 }
 
 int main() {
@@ -49,21 +47,18 @@ int main() {
         for (int j = 0; j < W; j++) cin >> S[i][j];
     }
 
-    priority_queue<Point> pq;
+    priority_queue<T, vector<T>, greater<T>> pq;
     ll now = S[P][Q];
     S[P][Q] = -1;  // 吸収済み
-    adj(P, Q, pq);
+    push(P, Q, pq);
 
     while (!pq.empty()) {
-        Point p = pq.top(); pq.pop();
-        if (S[p.x][p.y] == -1) continue;
-        if (over18(p.s, X)) break;  // 戦闘力のmaxは2.5*1e17
-        // over18により19桁以上になることはないが、18桁になることはあるのでlong longだと足りない可能性がある
-        if (ull(p.s) * X < now) {
-            now += p.s;
-            S[p.x][p.y] = -1;
-            adj(p.x, p.y, pq);
-        } else break;
+        auto [s, x, y] = pq.top(); pq.pop();
+        if (S[x][y] == -1) continue;
+        if (s >= (now + X - 1) / X) break;
+        now += s;
+        S[x][y] = -1;
+        push(x, y, pq);
     }
 
     cout << now << endl;
