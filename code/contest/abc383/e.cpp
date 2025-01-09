58416e0a653bfc88bb6b6a9f356b3e4df194c100
shyu61 <ohs6261@gmail.com>
2025-01-09 23:17:40 +0900

    Update

--- code/contest/abc383/e.cpp
+++ code/contest/abc383/e.cpp
@@ -2,34 +2,32 @@
 using namespace std;
 using ll = long long;
 
+// ジャンル
+// - 無向グラフ, 数列, 数式問題, 組み合わせ最適化, マッチング問題, パス最適化, 最大重みの最小化
+
+// 方針
+// グラフにおける最大重みの最小化とくれば、MST一択
+// MSTの構築過程は最大重みの最小化そのものなので、kruskal法を使いつつ、ペアリングしていくことを考える
+
+// 別の観点から見ると、数式問題なので(1)構成要素の解析と(2)数式の目的を考えるのも定石。(2)より組合せ最適化問題であることがわかる。
+// 組合せ最適化なので、全探索,貪欲法,dpあたりを検討する。全探索は明らかに無理で、dpも遷移の記述が難しそうなので可能性低い。
+// どちらにせよ「最小となる条件」をまず特定する必要があるので考える。明らかに最小のedgeのみで2点を結べる時に最小。この時点でedgeを基準に考えれば良いことがわかるし、まさにMSTだと気づけるはず。
+
 struct UnionFind {
     vector<int> par;
-
     UnionFind(int n) : par(n, -1) {}
     void init(int n) { par.assign(n, -1); }
-
-    int root(int x) {
-        if (par[x] < 0) return x;
-        else return par[x] = root(par[x]);  // 経路圧縮
-    }
-
-    bool same(int x, int y) {
-        return root(x) == root(y);
-    }
-
+    int root(int x) { return par[x] < 0 ? x : par[x] = root(par[x]); }
+    bool same(int x, int y) { return root(x) == root(y); }
     bool unite(int x, int y) {
         x = root(x), y = root(y);
         if (x == y) return false;
-        // 常に小さい木を大きい木に結合する
         if (par[x] > par[y]) swap(x, y);
         par[x] += par[y];
         par[y] = x;
         return true;
     }
-
-    int size(int x) {
-        return -par[root(x)];
-    }
+    int size(int x) { return -par[root(x)]; }
 };
 
 int main() {
@@ -40,14 +38,8 @@ int main() {
         edges[i] = {w, --u, --v};
     }
     vector<int> A(N), B(N);
-    for (int i = 0; i < K; i++) {
-        int a; cin >> a;
-        A[--a]++;
-    }
-    for (int i = 0; i < K; i++) {
-        int b; cin >> b;
-        B[--b]++;
-    }
+    for (int i = 0; i < K; i++) { int a; cin >> a; A[--a]++; }
+    for (int i = 0; i < K; i++) { int b; cin >> b; B[--b]++; }
 
     sort(edges.begin(), edges.end());
 
@@ -64,6 +56,7 @@ int main() {
             ans += ll(c) * x;
             A[u] -= x; B[v] -= x;
         }
+        // ペアを作った2頂点は縮約する(以降のループのedgeは必ず重みが大きくなるので、"パスの重み"は変更される)
         uf.unite(u, v);
         A[uf.root(u)] = A[u] + A[v];
         B[uf.root(u)] = B[u] + B[v];
