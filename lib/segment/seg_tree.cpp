3cf790d275952b2ce2478ec2a5d355df87c60019
shyu61 <ohs6261@gmail.com>
2025-01-13 22:16:40 +0900

    Update

--- lib/segment/seg_tree.cpp
+++ lib/segment/seg_tree.cpp
@@ -1,37 +1,24 @@
-// RMQの場合
 #include <bits/stdc++.h>
 using namespace std;
 
-int INF = numeric_limits<int>::max();
-struct RMQ {
+template<auto op, int iv>
+struct Segtree {
     vector<int> dat;
     int sz = 1;
 
-    RMQ() {}
+    Segtree() {}
     // n: 要素数, a: 初期化時に利用する値
-    RMQ(int n, const vector<int>& a) {
+    Segtree(int n, const vector<int>& a) {
         while (sz < n) sz *= 2;
-        dat = vector<int>(sz * 2 - 1);
+        dat = vector<int>(sz * 2 - 1, iv);
 
         for (int i = 0; i < n; i++) dat[i + sz - 1] = a[i];
         for (int i = sz - 2; i >= 0; i--) {
-            dat[i] = min(dat[i * 2 + 1], dat[i * 2 + 2]);
+            dat[i] = op(dat[i * 2 + 1], dat[i * 2 + 2]);
         }
     }
 
-    // [a, b)の最小値を探索
-    // k: ノード番号, [l, r)はそれに紐づく区間
-    int query(int a, int b, int k, int l, int r) {
-        // [a, b)と[l, r)が交差しない場合は解なし
-        if (b <= l || r <= a) return INF;
-        // [a, b)が[l, r)を完全に含んでいる時、このノードの値が解
-        if (a <= l && r <= b) return dat[k];
-        else {
-            int vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-            int vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
-            return min(vl, vr);
-        }
-    }
+    int query(int a, int b) { return query(a, b, 0, 0, sz); }
 
     // k: 要素番号, a: 更新値
     void update(int k, int a) {
@@ -40,7 +27,22 @@ struct RMQ {
         dat[k] = a;
         while (k > 0) {
             k = (k - 1) / 2;
-            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+            dat[k] = op(dat[k * 2 + 1], dat[k * 2 + 2]);
         }
     }
+
+    private:
+        // [a, b)の最小値を探索
+        // k: ノード番号, [l, r)はそれに紐づく区間
+        int query(int a, int b, int k, int l, int r) {
+            // [a, b)と[l, r)が交差しない場合は解なし
+            if (b <= l || r <= a) return iv;
+            // [a, b)が[l, r)を完全に含んでいる時、このノードの値が解
+            if (a <= l && r <= b) return dat[k];
+            else {
+                int vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+                int vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+                return op(vl, vr);
+            }
+        }
 };
--- run.sh
+++ run.sh
@@ -4,8 +4,13 @@ g++-14 -std=c++20 -I . -g -o main main.cpp
 
 awk 'BEGIN {RS="\n\n"} {f="chunk" NR ".txt"; print > f}' in.txt
 
-for chunk in chunk*.txt; do
-    cat "$chunk" | ./main
-done
+if [ $# -eq 1 ]; then
+    ./main < "chunk$1.txt"
+else
+    for chunk in chunk*.txt; do
+        ./main < "$chunk"
+        echo
+    done
+fi
 
 rm -r chunk*.txt
