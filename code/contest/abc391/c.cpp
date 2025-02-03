3651f7db9b96531a6849b8e6ed53f19846a2f420
shyu61 <ohs6261@gmail.com>
2025-02-03 18:54:28 +0900

    Update

---
+++ code/contest/abc391/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 方針
+// update: 鳩を探して移動する
+//   - 鳩がどこにいるかの位置配列: O(1)
+//   - 移動: O(1)
+// query: 複数いる巣の個数
+//   - 巣ごとの鳩の数: O(1)
+//   - 上記を使ってクエリ毎に、複数いる巣の個数を計算: O(1)
+
+// ポイント
+// 何を高速化する必要があるのかを分解して、1つずつ考える
+
+int main() {
+    int n, q; cin >> n >> q;
+    vector<int> a(n), cnt(n, 1);
+    int num = 0;
+    for (int i = 0; i < n; i++) a[i] = i;
+
+    while (q--) {
+        int type; cin >> type;
+        if (type == 1) {
+            int p, h; cin >> p >> h; p--; h--;
+            cnt[a[p]]--;
+            if (cnt[a[p]] == 1) num--;
+            a[p] = h;
+            cnt[a[p]]++;
+            if (cnt[a[p]] == 2) num++;
+        } else {
+            cout << num << '\n';
+        }
+    }
+}
