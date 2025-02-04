ae06079c26005105cb098dd7f93656d736008105
shyu61 <ohs6261@gmail.com>
2025-02-04 20:14:21 +0900

    Update

--- code/contest/abc391/e.cpp
+++ code/contest/abc391/e.cpp
@@ -7,6 +7,7 @@ using uint = unsigned int;
 // ジャンル
 // 再帰構造, ツリーdp
 
+// 木は構築せず各階層ごとにbottom-upでdpする方法
 int main() {
     int n; string s; cin >> n >> s;
     vector<vector<int>> dp(s.size(), vector<int>(2, 1));
@@ -25,6 +26,35 @@ int main() {
             }
         }
     }
+    int ans = max(dp[0][0], dp[0][1]);
+    cout << ans << endl;
+}
+
+// 三分木を構築してdfsする方法
+int main() {
+    int n; string s; cin >> n >> s;
+    int n3 = s.size();
+
+    vector<vector<int>> dp((3 * n3 - 1) / 2, vector<int>(2, INF));
+    int leaf = (n3 - 1) / 2;
+    auto dfs = [&](auto dfs, int v, int c) {
+        if (dp[v][c] != INF) return dp[v][c];
+        // 葉
+        if (v >= leaf) {
+            int x = s[v - leaf] - '0';
+            return abs(x - c);
+        }
+        rep(t, 8) {
+            int cost = 0;
+            rep(i, 3) cost += dfs(dfs, v * 3 + i + 1, t >> i & 1);
+            int x = popcount(uint(t)) >= 2 ? 1 : 0;
+            dp[v][x] = min(dp[v][x], cost);
+        }
+        return dp[v][c];
+    };
+
+    dfs(dfs, 0, 0);
+
     int ans = max(dp[0][0], dp[0][1]);
     cout << ans << endl;
 }
