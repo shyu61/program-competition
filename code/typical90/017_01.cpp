3b87edc5c5222e8cb1b95e68caa6f0459c9f8165
shyu61 <ohs6261@gmail.com>
2025-02-20 21:30:42 +0900

    Update

--- code/typical90/017_01.cpp
+++ code/typical90/017_01.cpp
@@ -5,8 +5,40 @@ using P = pair<int, int>;
 const int INF = 1001001001;
 
 // ジャンル
-// 数え上げ, 計算幾何
+// 数え上げ, 区間問題, 配置問題
 
+// ポイント
+// - 数え上げの条件式が複数ある場合は、補集合により簡単な条件に変換できないか考える
+// - 位置関係問題は、sortやswapにより条件をシンプルにすることを考える
+// - 区間や位置による数え上げは、fenwicktreeによる高速化ができないか検討する
+
+// 全探索による解法
+int main() {
+    int n, m; cin >> n >> m;
+    vector<P> s(m);
+    rep(i, m) {
+        int l, r; cin >> l >> r, l--, r--;
+        s[i] = {l, r};
+    }
+    sort(s.begin(), s.end());
+
+    auto is_intersect = [&](P& a, P& b) -> bool {
+        auto [ls, rs] = a;
+        auto [lt, rt] = b;
+        return ls < lt && lt < rs && rs < rt;
+    };
+
+    int ans = 0;
+    rep(i, m) {
+        for (int j = i + 1; j < m; j++) {
+            if (is_intersect(s[i], s[j])) ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+// 一部を二分探索で高速化する解法
+// R同士の比較が線形なので計算量は同じ。但し定数倍は小さくなる
 int main() {
     int n, m; cin >> n >> m;
     vector<P> s(m);
