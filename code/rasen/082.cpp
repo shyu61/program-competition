dae7bede60e79aaa569cdc711671aa76fbddeca2
shyu61 <ohs6261@gmail.com>
2025-03-04 22:36:42 +0900

    Update

--- code/rasen/082.cpp
+++ code/rasen/082.cpp
@@ -1,101 +1,57 @@
 // 線分交差問題
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <vector>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
-const double EPS = 1e-10;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+// 想定解
+// 平面走査
+
+// 罠
+// 同じ走査線に要素が複数ある場合の処理する順は要注意
+
+// ポイント
+// 平面走査は一方の軸についてsortし順に見ることで固定する。
+// もう一方は二分探索などで処理することでO(NlogN)で実行する
 
 struct Point {
-    double x, y;
-    Point(double x = 0, double y = 0) : x(x), y(y) {}
-    Point operator+(Point p) { return Point(x + p.x, y + p.y); }
-    Point operator-(Point p) { return Point(x - p.x, y - p.y); }
-    Point operator*(double k) { return Point(k * x, k * y); }
-    Point operator/(double k) { return Point(x / k, y / k); }
-    double norm() { return x * x + y * y; }
-    double abs() { return sqrt(norm()); }
+    int x, y;
+    Point() {}
+    Point(int x, int y) : x(x), y(y) {}
 };
-struct Segment {
-    Point p1, p2;
-    Segment() {}
-    Segment(Point p1, Point p2) : p1(p1), p2(p2) {}
-};
-struct Cycle { Point c; double r; };
-using Vector = Point;
-
-double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
-double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
-bool equals(double a, double b) { return fabs(a - b) < EPS; }
-
-// ==============================
-const int BOTTOM = 0;
-const int LEFT = 1;
-const int RIGHT = 2;
-const int TOP = 3;
-int n;
-
-struct Endpoint {
-    Point p;
-    int seg, st;
-    Endpoint() {}
-    Endpoint(Point p, int seg, int st): p(p), seg(seg), st(st) {}
-
-    bool operator<(const Endpoint &ep) const {
-        // y座標が小さい順にソート
-        if (p.y == ep.p.y) return st < ep.st;
-        return p.y < ep.p.y;
-    }
-};
-
-void solve(vector<Segment> segments, vector<Endpoint> endpoints) {
-    set<int> bst;
-    int cnt = 0;
-    sort(endpoints.begin(), endpoints.end());
-
-    for (int i = 0; i < endpoints.size(); i++) {
-        Endpoint ep = endpoints[i];
-        if (ep.st == BOTTOM) {
-            bst.insert(ep.p.x);
-        } else if (ep.st == LEFT) {
-            double x1 = segments[ep.seg].p1.x;
-            double x2 = segments[ep.seg].p2.x;
-            if (x1 > x2) swap(x1, x2);
-
-            set<int>::iterator it_l = bst.lower_bound(x1);
-            set<int>::iterator it_u = bst.upper_bound(x2);
-
-            cnt += distance(it_l, it_u);
-        } else if (ep.st == TOP) {
-            bst.erase(ep.p.x);
-        }
-    }
-
-    cout << cnt << endl;
-}
 
 int main() {
-    cin >> n;
-    vector<Segment> segments(n);
-    vector<Endpoint> endpoints(n * 2);
-    for (int i = 0; i < n; i++) {
-        cin >> segments[i].p1.x >> segments[i].p1.y >> segments[i].p2.x >> segments[i].p2.y;
+    int n; cin >> n;
+    vector<pair<Point, Point>> seg(n);
+    rep(i, n) {
+        int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        if (x1 > x2) swap(x1, x2);
+        if (y1 > y2) swap(y1, y2);
+        seg[i] = {Point(x1, y1), Point(x2, y2)};
+    }
 
-        if (segments[i].p1.x == segments[i].p2.x && segments[i].p1.y > segments[i].p2.y) {
-            swap(segments[i].p1, segments[i].p2);
-        }
-        if (segments[i].p1.y == segments[i].p2.y && segments[i].p1.x > segments[i].p2.x) {
-            swap(segments[i].p1, segments[i].p2);
-        }
-
-        if (segments[i].p1.x == segments[i].p2.x) {
-            endpoints[i * 2] = Endpoint(segments[i].p1, i, BOTTOM);
-            endpoints[i * 2 + 1] = Endpoint(segments[i].p2, i, TOP);
+    // type: 1=bottom, 2=other, 3=top
+    vector<tuple<int, int, int>> events;
+    rep(i, n) {
+        if (seg[i].first.x == seg[i].second.x) {
+            events.emplace_back(seg[i].first.y, 1, i);
+            events.emplace_back(seg[i].second.y, 3, i);
         } else {
-            endpoints[i * 2] = Endpoint(segments[i].p1, i, LEFT);
-            endpoints[i * 2 + 1] = Endpoint(segments[i].p2, i, RIGHT);
+            events.emplace_back(seg[i].first.y, 2, i);
         }
     }
-    solve(segments, endpoints);
+    sort(events.begin(), events.end());
+
+    int ans = 0;
+    set<int> st;
+    for (auto [ny, type, id] : events) {
+        if (type == 1) st.insert(seg[id].first.x);
+        if (type == 3) st.erase(seg[id].second.x);
+        if (type == 2) {
+            ans += distance(
+                st.lower_bound(seg[id].first.x),
+                st.upper_bound(seg[id].second.x)
+            );
+        }
+    }
+    cout << ans << endl;
 }
