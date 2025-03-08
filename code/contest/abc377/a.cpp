9833ec57957eadba70cfc70243c8759fe154897f
shyu61 <ohs6261@gmail.com>
2025-03-08 18:03:54 +0900

    Update

--- code/contest/abc377/a.cpp
+++ code/contest/abc377/a.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int main() {
     string s; cin >> s;
-    vector<int> cnt(3);
-    rep(i, 3) {
-        if (s[i] == 'A') cnt[0]++;
-        if (s[i] == 'B') cnt[1]++;
-        if (s[i] == 'C') cnt[2]++;
-    }
-
-    if (cnt[0] == 1 && cnt[1] == 1 && cnt[2] == 1) cout << "Yes" << endl;
+    sort(s.begin(), s.end());
+    if (s == "ABC") cout << "Yes" << endl;
     else cout << "No" << endl;
 }
--- code/contest/abc377/c.cpp
+++ code/contest/abc377/c.cpp
@@ -4,6 +4,9 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+// ポイント
+// sparseな既出管理はmapではなくsetで十分
+
 const vector<int> dx = {2,1,-1,-2,-2,-1,1,2}, dy = {1,2,2,1,-1,-2,-2,-1};
 
 int main() {
@@ -13,24 +16,16 @@ int main() {
         int a, b; cin >> a >> b, a--, b--;
         p[i] = {a, b};
     }
-    sort(p.begin(), p.end());
 
-    map<P, bool> used;
-    ll ans = ll(n) * n - m;
+    set<P> st;
     rep(i, m) {
-        // (a[i],b[i])が取れる範囲にある空きマスの数を数える
-        int sum = 0;
+        st.insert(p[i]);
         rep(r, 8) {
             int nh = p[i].first + dx[r], nw = p[i].second + dy[r];
             if (nh < 0 || nh >= n || nw < 0 || nw >= n) continue;
-            P np = {nh, nw};
-            int x = lower_bound(p.begin(), p.end(), np) - p.begin();
-            if (x <= m && p[x] != np && !used[np]) {
-                sum++;
-                used[np] = true;
-            }
+            st.emplace(nh, nw);
         }
-        ans -= sum;
     }
+    ll ans = ll(n) * n - st.size();
     cout << ans << endl;
 }
