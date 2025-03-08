9c67c87083f3000d742c05a08a171b96d6190055
shyu61 <ohs6261@gmail.com>
2025-03-08 17:26:48 +0900

    Update

--- code/contest/abc377/d.cpp
+++ code/contest/abc377/d.cpp
@@ -2,34 +2,30 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using ll = long long;
-using P = pair<int, int>;
+
+// 想定解
+// 右区間を固定して尺取り法
+
+// 罠
+// 各区間の終点に注目すればいいことは簡単に思いつけるが、それでループすると実装が複雑になる点
+// → 位置配列として記録しmについてループする必要がある点
+
+// ポイント
+// 求めるものの種類によってループ対象を変えるとよい
+// - 数え上げの場合は連続的に走査すると考えやすい
 
 int main() {
     int n, m; cin >> n >> m;
-    vector<int> l(n), r(n);
-    rep(i, n) cin >> l[i] >> r[i];
-
-    vector<P> events(n);
-    rep(i, n) events[i] = {r[i], l[i]};
-    sort(events.begin(), events.end());
-
-    int s = 1; ll ans = 0;
+    vector<int> maxl(m + 1, -1);
     rep(i, n) {
-        auto [rx, lx] = events[i];
-        // rxに辿り着いた, 区間を計算してsをlxまで進める
-        if (lx < s) continue;
-        int d = min(rx, m) - 1 - s + 1;
-        if (d > 0) {
-            ans += ll(d) * (d - 1) / 2 + d;
-        }
-        s = lx + 1;
-        int d2 = min(rx, m) - 1 - s + 1;
-        if (d2 > 0) ans -= ll(d2) * (d2 - 1) / 2 + d2;
-        if (s > m) break;
+        int l, r; cin >> l >> r;
+        maxl[r] = max(maxl[r], l);
     }
-    if (s <= m) {
-        int d = m - s + 1;
-        ans += ll(d) * (d - 1) / 2 + d;
+
+    int l = 1; ll ans = 0;
+    for (int r = 1; r <= m; r++) {
+        if (l <= maxl[r]) l = maxl[r] + 1;
+        ans += r - l + 1;
     }
     cout << ans << endl;
 }
