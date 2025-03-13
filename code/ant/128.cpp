043d7ba7c4246eb510c8b03d03538abcd2c38d21
shyu61 <ohs6261@gmail.com>
2025-03-13 21:34:39 +0900

    Update

--- code/ant/128.cpp
+++ code/ant/128.cpp
@@ -1,26 +1,30 @@
 // Nim(Georgia and Bob): p278
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
 // Nimへの帰着を検討すべき問題
-// - 複数の独立した部分集合から成る場合
-// - 各部分集合を操作可能なリソースとみなせること
-// - 終了条件がリソースが尽きることに該当すること
+// - 組合せゲームかつ不偏ゲームである場合
+//   - 確定性, 完全情報性, 零和, 有限, 操作対称性(同盤面ならプレイヤー間で行える操作に差異がない)
+// - 公平ゲームである
+//   - 同盤面ならプレイヤー間で行える操作に差異がない
+// - 独立したサブゲームの集合から成る場合
+// - 状態が減少する操作のみ許容され、有限回で決着する
+//   - 増加操作を必ず相殺できる、操作が有限回に限られるなどの場合はその限りではない
 
 int main() {
     int n; cin >> n;
     vector<int> p(n);
-    for (int i = 0; i < n; i++) cin >> p[i];
-
+    rep(i, n) cin >> p[i];
     sort(p.begin(), p.end());
 
-    int x = 0;
-    for (int i = 0; i + 1 < n; i += 2) {
+    int xo = 0;
+    for (int i = 0; i < n - 1; i += 2) {
         // 2駒間の距離を石の数と考えてNimに帰着
-        x ^= (p[i + 1] - p[i] - 1);
+        xo ^= (p[i + 1] - p[i] - 1);
     }
-    if (n % 2 == 1) x ^= p[0] - 1;
+    if (n % 2 == 1) xo ^= p[0] - 1;
 
-    cout << (x != 0 ? "Georgia will win" : "Bob will win") << endl;
+    if (xo != 0) cout << "Georgia will win" << endl;
+    else cout << "Bob will win" << endl;
 }
