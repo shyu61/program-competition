56e0dd6ecbe11a7fc4cfa18c67b556399fac7f29
shyu61 <ohs6261@gmail.com>
2025-04-20 15:04:43 +0900

    Update

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,14 +4,19 @@ set -eu
 
 target=${1:-problems}
 
-if [ $target = 'contest' ]; then
-    con=${2}
-    [ ! -d ./code/contest/${con} ] && mkdir ./code/contest/${con}
-    mv main.cpp ./code/contest/${con}/${3}.cpp
+cleanup() {
+    mv main.cpp "${1}"
     cp base.cpp main.cpp
     sed -i '' '/atcoder/d' main.cpp
     sed -i '' '/INF/d' main.cpp
+    sed -i '' '/\/\//d' main.cpp
     rm -f main
+}
+
+if [ "$target" = 'contest' ]; then
+    path="./code/contest/${2}"
+    [ -d "${path}" ] || mkdir "${path}"
+    cleanup "${path}/${3}.cpp"
     exit 0
 fi
 
@@ -19,8 +24,4 @@ last_num=$(find ./code/${target} -type f -name '*.cpp' | sed -E 's/.*\/([0-9]+)\
 new_number=$((10#$last_num+1))
 filename=$(printf "%03d" ${new_number})
 
-mv main.cpp ./code/${target}/${filename}.cpp
-cp base.cpp main.cpp
-sed -i '' '/atcoder/d' main.cpp
-sed -i '' '/INF/d' main.cpp
-rm -f main
+cleanup "./code/${target}/${filename}.cpp"
