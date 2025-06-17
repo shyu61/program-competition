5e1d1fab2379ca13225d73e4d48ef61d21a056d9
shyu61 <ohs6261@gmail.com>
2025-06-17 16:54:14 +0900

    Update

--- code/contest/abc410/b.cpp
+++ code/contest/abc410/b.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
-using ll = long long;
-using P = pair<int, int>;
 const int INF = 1001001001;
 
 int main() {
--- code/contest/abc410/c.cpp
+++ code/contest/abc410/c.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
-using ll = long long;
-using P = pair<int, int>;
 
 int main() {
     int n, q; cin >> n >> q;
