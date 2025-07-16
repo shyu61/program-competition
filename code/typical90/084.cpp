54e0a43ed63bab0687ea3707344f2c39d0bff9dd
shyu61 <ohs6261@gmail.com>
2025-07-16 18:37:24 +0900

    Update

--- code/typical90/084.cpp
+++ code/typical90/084.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using ll = long long;
 
+// next配列を使った解法
 int main() {
     int n; string s; cin >> n >> s;
 
@@ -18,5 +19,24 @@ int main() {
 
     ll ans = 0;
     for (auto v : nx) ans += n - v;
+    cout << ans << endl;
+}
+
+// ランレングス圧縮を使った解法
+int main() {
+    int n; string s; cin >> n >> s;
+    s += '#';  // 番兵
+
+    vector<int> a;
+    int l = 0;
+    rep(i, n) {
+        if (s[i] != s[i + 1]) {
+            a.push_back(i - l + 1);
+            l = i + 1;
+        }
+    }
+
+    ll ans = ll(n) * (n + 1) / 2;
+    for (auto i : a) ans -= ll(i) * (i + 1) / 2;
     cout << ans << endl;
 }
