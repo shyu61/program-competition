a948c691cd070a71a24d306f18ec4885ce51eef3
shyu61 <ohs6261@gmail.com>
2026-06-24 01:50:39 +0900

    Add palindrome checker solution

--- main.cpp
+++ main.cpp
@@ -4,4 +4,22 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-int main() {}
+bool is_palindrome(string s) {
+    string t = s;
+    reverse(t.begin(), t.end());
+    return s == t;
+}
+
+int main() {
+    string s; cin >> s;
+    int n = s.size();
+
+    string a = s.substr(0, (n-1)/2);
+    string b = s.substr((n+3)/2-1);
+
+    if (is_palindrome(s) && is_palindrome(a) && is_palindrome(b)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
